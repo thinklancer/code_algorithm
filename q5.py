c14b9e2f54ecb8478e11c3199d1dbd898f8004c6
thinklancer <thinklancer@gmail.com>
2013-01-18 00:13:21 +0100

    improve efficiency
    print out process
    reduce search length

--- q5.py
+++ q5.py
@@ -7,6 +7,16 @@
     http://www.tsp.gatech.edu/world/countries.html
 '''
 
+'''
+    Let A = 2D array, indexed by subsets S in {1,2,...,n} that contain 1 and destinations j in {1,2,...n}
+	Base case: A[S,1] = 0 if S={1}  or infinity otherwise
+	for m=2,3,4,...,n:
+    for each set S in {1,2,...,n} of size m that contains 1:
+    for each j in S, j!=1:
+    A[S,j]=min_{k in S and k!=j}  { A[S-{j},k]+Ckj }
+	return min_{j=2,...,m} {A[{1,2,3,...,n},j]+Cj1}
+'''
+
 import numpy as np
 import sys
 
@@ -14,6 +24,8 @@ LARGENUMBER =  1e20
 
 def builddist(ncity,x,y):
     distance = np.zeros((ncity,ncity))
+    x = x-min(x)
+    y = y-min(y)
     for i in range(ncity):
         for j in range(i,ncity):
             distance[i,j]=distance[j,i]=np.sqrt((x[i]-x[j])**2+(y[i]-y[j])**2)
@@ -55,20 +67,34 @@ def search(dist,od,ncity):
     A = np.zeros((nset,ncity))+LARGENUMBER
     A[0,0] = 0
     index = 1
+    firstpart = 1
+    oldelement = 1
     for m in range(1,ncity): # loop all subproblem size
-        nelement=comb(ncity-1,m)
-        print "processing ..",m,'/',ncity
+        nelement=np.int(comb(ncity-1,m))
+        print "processing ..",m,'/',ncity," for ",nelement,"cases"
+        process = 10
         for i in range(nelement): # loop all subset S with size m
+            if (i*100)/nelement > process:
+                print "      finish ",process,'%'
+                process+=10
             for j in range(1,ncity):
                 if bin(od[index]).zfill(ncity+1)[-j] == '1': # check j in S
                     temp = np.zeros(ncity)+LARGENUMBER
-                    target = np.where(od == od[index]-2**(j-1)) # S-{j} case
+                    #print nelement,od[0:oldelement]
+                    target = np.where(od[0:oldelement] == od[index]-2**(j-1)) # S-{j} case
+                    #print target,od[0:oldelement],od,od[index]-2**(j-1)
                     temp[0] = A[target,0]+dist[0,j]
                     for k in range(1,ncity):
                         if bin(od[index]).zfill(ncity+1)[-k] == '1': # check k in S
                             temp[k]=A[target,k]+dist[k,j]
                     A[index,j]=min(temp)
             index+=1
+        # remove excess part
+        index -= firstpart
+        A=np.delete(A,np.s_[0:firstpart],0)
+        od=np.delete(od,np.s_[0:firstpart],0)
+        firstpart = nelement
+        oldelement = nelement
     temp = np.zeros(ncity)+LARGENUMBER
     for i in range(1,ncity):
         temp[i]=A[-1,i]+dist[0,i]
