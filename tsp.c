383c3a67ade39cfecdcbdce996e6808a331f4417
thinklancer <thinklancer@gmail.com>
2013-01-19 16:41:14 +0100

    correct code but large memory required

--- tsp.c
+++ tsp.c
@@ -19,6 +19,7 @@ long loadtable(char *fname, float ***pos)
     *pos = (float **)malloc(n*sizeof(float*));
     for (i=0;i<n;i++)
     {
+        fgets(line,500,fp);
         (*pos)[i] = (float*)malloc(2*sizeof(float));
         sscanf(line,"%f %f",&((*pos)[i][0]),&((*pos)[i][1]));
     }
@@ -34,54 +35,48 @@ void computedist(int n, float**pos, float***dis)
     {
         (*dis)[i] = (float *)malloc(n*sizeof(float));
         for(j=0;j<n;j++)
+        {
             (*dis)[i][j]=sqrt(pow(pos[i][0]-pos[j][0],2)+pow(pos[i][1]-pos[j][1],2));
+        }
     }
 }
 
 void buildset(long n,long *set)
 {
-    long i,ne,p,count;
-    long *temp,j;
+    long i,ne,p,count,j,index;
     ne = pow(2,n-1);
-    temp = (long*)malloc(sizeof(long)*ne);
 
-    for(i=0;i<ne;i++)
-    {   
-        count=0;
-        for(p=1;p<=i;p=p<<1)
-            if (i&p)
-                ++count;
-        set[i]=count;
-        temp[i]=i;
-    }
-    // sort set by count and return index: very slow!!
-    i=ne;
-    while(i>0)
+
+    index=0;
+    for(j=0;j<=n;j++)
     {
-        for(j=0;j<i-1;j++)
+        for(i=0;i<ne;i++)
         {
-            if(set[j]>set[j+1])
+            count=0;
+            for(p=1;p<=i;p=p<<1)
+                if (i&p)
+                    ++count;
+            if(count==j)
             {
-                p = set[j];
-                set[j]=set[j+1];
-                set[j+1]=p;
-                p = temp[j];
-                temp[j] = temp[j+1];
-                temp[j+1]=p;
+                set[index]=i;
+                index++;
             }
         }
     }
-    for(i=0;i<ne;i++)
-        set[i]=temp[i];
-    free(temp);
+//    for (i=0;i<ne;i++) printf("%ld ",set[i]);
+//    printf("\n");
 }
 
 void search(float **dis, long *set, long n)
 {
-    long nset,nelement,i,j,m,index,nm;
+    long nset,nelement,i,j,k,m,index,nm,target;
     double **a;
+    double *len,min;
+    int it;
+    
     nset = (long)(pow(2,n-1));
     a = (double**)malloc(n*sizeof(double));
+    len = (double*)malloc(n*sizeof(double));
     for(i=0;i<n;i++)
     {
         a[i] = (double*)malloc(nset*sizeof(double));
@@ -94,31 +89,52 @@ void search(float **dis, long *set, long n)
     for (m=1;m<n;m++) // loop all subproblem size
     {
         nelement=1;
-        nm = m<n-m?m:n-m;
+        nm = m<n-m-1?m:n-m-1;
         for (i=0;i<nm;i++)
             nelement*=n-i-1;
         for (i=0;i<nm;i++)
             nelement/=i+1;
-        printf("Processing .. %ld / %ld, for %ld cases\n",m,n,nelement);
+        printf("Processing .. %ld / %ld for %ld cases\n",m,n,nelement);
         for (i=0;i<nelement;i++)
         {
             for(j=1;j<n;j++)
             {
                 // check j in subset set[index]
-                if (set[index] & (long)(pow(2,j)))
+                if (set[index] & (long)(pow(2,j-1)))
                 {
-                    printf("%ld %ld %ld %ld\n",i,j,set[index],(long)(pow(2,j)));
+                    //printf("%ld %ld %ld\n",index,set[index],(long)(pow(2,j)));
                     // NEED TO FINISH!!
+                    for (it=0;it<n;it++) len[it]=LARGENUM;
+                    target = set[index]-(long)(pow(2,j-1));
+                    len[0]=a[0][target]+dis[0][j];
+                    for(k=1;k<n;k++)
+                        if (set[index] & (long)(pow(2,k-1))) // check k in set[index]
+                        {
+                            len[k]=a[k][target]+dis[k][j];
+                        }
+                    min=LARGENUM;
+                    for (k=0;k<n;k++)
+                        if (len[k]<min) min=len[k];
+                    a[j][set[index]]=min;
                 }
             }
             index++;
         }
     }
-    
+    for (it=0;it<n;it++) len[it]=LARGENUM;
+    for(i=1;i<n;i++)
+    {
+        len[i]=a[i][nset-1]+dis[0][i];
+    }
+    min = LARGENUM;
+    for (k=0;k<n;k++)
+        if (len[k]<min) min=len[k];
+    printf("%lf\n",min);
     // clean up
     for(i=0;i<n;i++)
         free(a[i]);
     free(a);
+    free(len);
 }
 
 int main(int argc, char*argv[])
